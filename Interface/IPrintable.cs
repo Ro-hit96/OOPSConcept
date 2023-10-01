f6123122aa8ef2144712bbee4de9b756c165c364
RO-HITT <rsurvase1132@gmail.com>
2023-10-01 21:02:39 +0530

    ..

--- Interface/IPrintable.cs
+++ Interface/IPrintable.cs
@@ -8,13 +8,29 @@ namespace OOPSConcept.Interface
 {
     public interface IPrintable
     {
-        string print();//bydefault public and abstract
+        void print();
+        //bydefault public and abstract
+        void display();
     }
     public class User : IPrintable
     {
-        public string print()
+        public void print()
         {
-            return "User Details";
+            Console.WriteLine("Print Method");
+          
+        }
+        public void display()
+        {
+            Console.WriteLine("Display Method");
+        }
+    }
+    public class UserMMInter
+    {
+        static void Main(string[] args)
+        {
+            User u1= new User();
+            u1.print();
+            u1.display();
         }
     }
 }
---
+++ MethoLoad.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPSConcept
+{
+    public class MethoLoad
+    {
+
+        public int add(int a,int b)
+        {
+            return a+b;
+        }
+        public void add(double v,double c)
+        {
+            double d = v + c;
+          
+        }
+        public int add(int a,int b,int c)
+        {
+            return a+b+c;
+        }
+        
+    }
+    public class loadins
+    {
+        static void Main(string[] args)
+        {
+            MethoLoad m=new MethoLoad();
+            m.add(1,2);
+            m.add(22.2, 22);
+            m.add(2,34,4);
+        }
+    }
+}
